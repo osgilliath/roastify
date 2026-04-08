e565c01625ecf38892c4c0671db57594d323ffed
osgilliath <shankhdhar.madhav@gmail.com>
2026-04-08 23:59:28 +0530

    fix: port error

--- backend/main.py
+++ backend/main.py
@@ -84,5 +84,5 @@ async def get_roast(authorization: str = Header(None)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
